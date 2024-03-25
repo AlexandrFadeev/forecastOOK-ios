c6a1127f095e3a18a3b97ecdb362bef0258bba29
Alexandr Fadeev <alex@Alexandrs-MacBook-Pro.local>
2024-03-25 19:59:09 +0200

    created empty swiftUI project

---
+++ ForecastOOK/ForecastOOKApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForecastOOKApp.swift
+//  ForecastOOK
+//
+//  Created by Alexandr Fadeev on 25.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ForecastOOKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
